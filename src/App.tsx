a70168b7caed1574a5b3fd6fca29329cd94a639f
Guillem Ribes Espurz <gribes03@gmail.com>
2025-09-25 22:31:45 +0200

    Deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/guillem-melania/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
