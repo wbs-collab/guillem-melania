f30f09afd25c6d057816862a598447c762220835
Guillem Ribes Espurz <gribes03@gmail.com>
2025-09-25 22:36:54 +0200

    Fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -15,7 +15,7 @@ i18n
       caches: ["cookie"],
     },
     backend: {
-      loadPath: "/locales/{{lng}}/translation.json",
+      loadPath: import.meta.env.BASE_URL + "locales/{{lng}}/translation.json",
     },
     react: {
       useSuspense: false,
